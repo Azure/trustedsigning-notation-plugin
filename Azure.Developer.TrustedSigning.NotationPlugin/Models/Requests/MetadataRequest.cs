cb1202ae1dc8806efed9d7d5de58acdcbdaa7ab9
Jaxel Rojas <jrojaslopez@microsoft.com>
2025-02-14 10:46:29 -0400

    Initial version of the trusted signing notation plugin (#7)
    * feat: initial version of the trusted signing notation plugin
    * refactor: mark compute hash var as byte[] to match func return
    * refactor: swap key responses types to string from string?
    * refactor: swap type for non nullable versions since theyre required
    * refactor: added algorithm messages to error output
    * refactor: mark props as required for signature response
    * refactor: moved from default values to required props
    * fix: remove deprecated files from solution
    * style: rebranding plugin to azure-trustedsigning

---
+++ Azure.Developer.TrustedSigning.NotationPlugin/Models/Requests/MetadataRequest.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Azure.Developer.TrustedSigning.NotationPlugin.Models;
+
+/// <summary>
+/// Documentation: https://github.com/notaryproject/specifications/blob/main/specs/plugin-extensibility.md#plugin-metadata
+/// </summary>
+internal class MetadataRequest : BaseRequest;
